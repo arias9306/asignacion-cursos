610fc140e2aab66899734e3f4a35142145282a40
EDIJAI <edinson.tk26@gmail.com>
2018-10-10 20:32:08 -0500

    add template

--- asignacion-salones/src/main/java/com/ucc/asignacion/controllers/SalonController.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/controllers/SalonController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ucc.asignacion.entities.Usuario;
 import com.ucc.asignacion.services.ISalonService;
-import com.ucc.asignacion.services.IUsuarioService;
 
 @Controller
 @RequestMapping("/salon")
