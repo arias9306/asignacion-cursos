6c607c57c9bd424da9805ad6a7312c30d0dff214
EDIJAI <edinson.tk26@gmail.com>
2018-10-10 21:21:28 -0500

    Exception

--- asignacion-salones/src/main/java/com/ucc/asignacion/controllers/ExceptionController.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/controllers/ExceptionController.java
@@ -5,12 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ExceptionController {
 
-	@ExceptionHandler(NoHandlerFoundException.class)
 	public ModelAndView handleError404(HttpServletRequest request, Exception e) {
 		System.out.println("123");
 		return new ModelAndView("/errores/404");
