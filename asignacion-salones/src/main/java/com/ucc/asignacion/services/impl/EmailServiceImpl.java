3615141eedbb223d58d95367255a34b2cedbbd06
SamsungPC\Samsung <rcastellanos1026@gmail.com>
2018-10-18 19:12:19 -0500

    SSL correos

--- asignacion-salones/src/main/java/com/ucc/asignacion/services/impl/EmailServiceImpl.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/services/impl/EmailServiceImpl.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Component;
 public class EmailServiceImpl {
 
 	private final JavaMailSender emailSender;
-	
+
 	@Autowired
 	public EmailServiceImpl(JavaMailSender emailSende) {
 		this.emailSender = emailSende;
 	}
-	
-	public void enviarCorreo(String to) {
-		SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setTo(to); 
-        message.setSubject("Recuperacion Password"); 
-        message.setText("Mk le envie un correo");
-        emailSender.send(message);
+
+	public void enviarCorreo(String to, String password) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject("Recuperacion Password");
+		message.setText("Mk le envie un correo " + to + " y la contraseña es: " + password);
+		emailSender.send(message);
 	}
 
 }
