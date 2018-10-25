af83c19ff703cbe96e6e82ad98607f5a4238c7d5
BUC-WLI70515HPF\Estudiantes <JLancherosRueda@gmail.com>
2018-10-24 21:31:59 -0500

    Validaciones en el formulario de usuarios

--- asignacion-salones/src/main/java/com/ucc/asignacion/controllers/UsuarioController.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/controllers/UsuarioController.java
@@ -1,8 +1,15 @@
 package com.ucc.asignacion.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,22 +41,40 @@ public class UsuarioController {
 	
 	@GetMapping("/edit/{id}")
 	public String editProduct(Model model, @PathVariable(value = "id") String id) {
-		model.addAttribute("usuarioModel", usuarioService.buscarUsuarioById(id));
+		UsuarioModel usuarioModel = usuarioService.buscarUsuarioById(id);
+		usuarioModel.setEditar(true);
+		model.addAttribute("usuarioModel", usuarioModel);
 		return "/usuarios/edit";
 	}
 	
 	@GetMapping("/add")
 	public ModelAndView createProduct(Model model) {
 		ModelAndView view = new ModelAndView("/usuarios/edit");
-		view.addObject("usuarioModel", new UsuarioModel());
+		view.addObject("usuarioModel", new UsuarioModel()); 
 		return view;
 	}
 	
 	@PostMapping("/")
-	public String saveProduct(UsuarioModel usuario) {
-		usuarioService.guardarUsuario(usuario);
-		return "redirect:/usuario/";
+	public ModelAndView saveProduct(@Valid UsuarioModel usuario, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			List<String> errors = new ArrayList<>();
+			for (ObjectError error : bindingResult.getAllErrors()) {
+				errors.add(error.getDefaultMessage());
+			}
+
+			ModelAndView view = new ModelAndView("/usuarios/edit");
+			view.addObject("usuarioModel", usuario);
+			view.addObject("errors", errors);
+			return view;
+			
+		} else {
+			usuarioService.guardarUsuario(usuario);
+			return new ModelAndView("redirect:/usuario/");
+			
+		}
+		
 	}
+			
 	
 	@GetMapping("/delete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") String id) {
--- asignacion-salones/src/main/java/com/ucc/asignacion/models/UsuarioModel.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/models/UsuarioModel.java
@@ -1,23 +1,54 @@
 package com.ucc.asignacion.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UsuarioModel {
-	
+
+	@NotNull(message = "El correo no puede ser nulo..")
+	@NotBlank(message = "El correo no puede estar vacio..")
+	@Email(message = "Debe ingresar un correo valido")
 	private String correo;
+	@NotNull(message = "La contraseña no puede ser nula..")
+	@NotBlank(message = "La contraseña no puede estar vacia..")
 	private String password;
 	private int idUser;
+	@NotNull(message = "El código no puede ser nulo..")
 	private int codigo;
-	private int  idRol;
+	@NotNull(message = "El rol no puede ser nulo..")
+	private int idRol;
+	@NotNull(message = "El programa no puede ser nulo..")
 	private int idPrograma;
+	@NotNull(message = "El número de identificación no puede ser nulo..")
 	private Integer nroIdentificacion;
-	private String estado, telefono;
-	private String primerApellido, segundoApellido, primerNombre, segundoNombre;
+	@NotNull(message = "El estado no puede ser nulo..")
+	@NotBlank(message = "El estado no puede estar vacio..")
+	private String estado;
+	@NotNull(message = "El teléfono no puede ser nulo..")
+	@NotBlank(message = "El teléfono no puede estar vacio..")
+	@Size(min = 7, max = 10, message = "El teléfono no es valido")
+	@Pattern(regexp = "[0-9]+", message = "Ingresar solamente números")
+	private String telefono;
+	@NotNull(message = "El primer apellido no puede ser nulo..")
+	@NotBlank(message = "El primer apellido no puede estar vacio..")
+	private String primerApellido;
+	private String segundoApellido;
+	@NotNull(message = "El primer nombre no puede ser nulo..")
+	@NotBlank(message = "El primer nombre no puede estar vacio..")
+	private String primerNombre;
+	private String segundoNombre;
 	private boolean cambioPassw;
-	
+	private boolean editar;
+
 	public UsuarioModel() {
-		
-		
+
 	}
-	
+
 	public UsuarioModel(int idUser, String primerApellido, String segundoApellido, String primerNombre,
 			String segundoNombre) {
 		super();
@@ -27,7 +58,6 @@ public class UsuarioModel {
 		this.primerNombre = primerNombre;
 		this.segundoNombre = segundoNombre;
 	}
-	
 
 	public int getCodigo() {
 		return codigo;
@@ -41,33 +71,25 @@ public class UsuarioModel {
 		return idRol;
 	}
 
-
 	public void setIdRol(int idRol) {
 		this.idRol = idRol;
 	}
-
 
 	public int getIdPrograma() {
 		return idPrograma;
 	}
 
-
 	public void setIdPrograma(int idPrograma) {
 		this.idPrograma = idPrograma;
 	}
-
 
 	public Integer getNroIdentificacion() {
 		return nroIdentificacion;
 	}
 
-
 	public void setNroIdentificacion(Integer nroIdentificacion) {
 		this.nroIdentificacion = nroIdentificacion;
 	}
-
-
-	
 
 	public String getTelefono() {
 		return telefono;
@@ -80,63 +102,82 @@ public class UsuarioModel {
 	public String getPrimerApellido() {
 		return primerApellido;
 	}
+
 	public void setPrimerApellido(String primerApellido) {
 		this.primerApellido = primerApellido;
 	}
+
 	public String getSegundoApellido() {
 		return segundoApellido;
 	}
+
 	public void setSegundoApellido(String segundoApellido) {
 		this.segundoApellido = segundoApellido;
 	}
+
 	public String getPrimerNombre() {
 		return primerNombre;
 	}
+
 	public void setPrimerNombre(String primerNombre) {
 		this.primerNombre = primerNombre;
 	}
+
 	public String getSegundoNombre() {
 		return segundoNombre;
 	}
+
 	public void setSegundoNombre(String segundoNombre) {
 		this.segundoNombre = segundoNombre;
 	}
+
 	public String getCorreo() {
 		return correo;
 	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public int getIdUser() {
 		return idUser;
 	}
+
 	public void setIdUser(int idUser) {
 		this.idUser = idUser;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
 	public boolean isCambioPassw() {
 		return cambioPassw;
 	}
+
 	public void setCambioPassw(boolean cambioPassw) {
 		this.cambioPassw = cambioPassw;
 	}
-	
-	
-	
-	
+
+	public boolean isEditar() {
+		return editar;
+	}
+
+	public void setEditar(boolean editar) {
+		this.editar = editar;
+	}
 
 	
-	
-
 }
