0ae2ea4f328b863971a835180e05dcfe2f20cf3b
BUC-WLI70515HPF\Estudiantes <JLancherosRueda@gmail.com>
2018-11-13 20:28:45 -0500

    Correcciones de módulos

--- asignacion-salones/src/main/java/com/ucc/asignacion/controllers/UsuarioController.java
+++ asignacion-salones/src/main/java/com/ucc/asignacion/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -25,110 +26,112 @@ import com.ucc.asignacion.services.IUsuarioService;
 @RequestMapping("/admin/usuario")
 public class UsuarioController {
 
-  private final IUsuarioService usuarioService;
-  private final IRolService rolService;
-  private final IProgramaService programaService;
+	private final IUsuarioService usuarioService;
+	private final IRolService rolService;
+	private final IProgramaService programaService;
 
-  private static final String REDIRECT_USUARIO = "redirect:/admin/usuario/";
-  private static final String VISTA_CONSULTA = "/admin/usuarios/usuario";
-  private static final String VISTA_EDITAR = "/admin/usuarios/edit";
-  private static final String REDIRECT_CAMBIARCONTRA = "redirect:/user/cambiarcontrasena";
+	private static final String REDIRECT_USUARIO = "redirect:/admin/usuario/";
+	private static final String VISTA_CONSULTA = "/admin/usuarios/usuario";
+	private static final String VISTA_EDITAR = "/admin/usuarios/edit";
+	private static final String REDIRECT_CAMBIARCONTRA = "redirect:/user/cambiarcontrasena";
 
-  /**
-   * Constructor.
-   *
-   * @param usuarioService
-   * @param rolService
-   * @param programaService
-   */
-  @Autowired
-  public UsuarioController(IUsuarioService usuarioService, IRolService rolService, IProgramaService programaService) {
-    this.rolService = rolService;
-    this.usuarioService = usuarioService;
-    this.programaService = programaService;
-  }
+	/**
+	 * Constructor.
+	 *
+	 * @param usuarioService
+	 * @param rolService
+	 * @param programaService
+	 */
+	@Autowired
+	public UsuarioController(IUsuarioService usuarioService, IRolService rolService, IProgramaService programaService) {
+		this.rolService = rolService;
+		this.usuarioService = usuarioService;
+		this.programaService = programaService;
+	}
 
-  @GetMapping("/")
-  public ModelAndView getUsuarios() {
-    ModelAndView view = new ModelAndView(VISTA_CONSULTA);
-    view.addObject("usuarios", usuarioService.usuarios());
-    return view;
-  }
+	@GetMapping("/")
+	public ModelAndView getUsuarios() {
+		ModelAndView view = new ModelAndView(VISTA_CONSULTA);
+		view.addObject("usuarios", usuarioService.usuarios());
+		return view;
+	}
 
-  @GetMapping("/edit/{id}")
-  public ModelAndView edit(@PathVariable(value = "id") String id) {
-    ModelAndView view = new ModelAndView(VISTA_EDITAR);
-    UsuarioModel usuarioModel = usuarioService.buscarUsuarioById(id);
-    usuarioModel.setEditar(true);
-    view.addObject("usuarioModel", usuarioModel);
-    view.addObject("rolModel", rolService.roles());
-    view.addObject("programaModel", programaService.programas());
-    return view;
-  }
+	@GetMapping("/edit/{id}")
+	public ModelAndView edit(@PathVariable(value = "id") String id) {
+		ModelAndView view = new ModelAndView(VISTA_EDITAR);
+		UsuarioModel usuarioModel = usuarioService.buscarUsuarioById(id);
+		usuarioModel.setEditar(true);
+		view.addObject("usuarioModel", usuarioModel);
+		view.addObject("rolModel", rolService.roles());
+		view.addObject("programaModel", programaService.programas());
+		return view;
+	}
 
-  @GetMapping("/add")
-  public ModelAndView create() {
-    ModelAndView view = new ModelAndView(VISTA_EDITAR);
-    view.addObject("usuarioModel", new UsuarioModel());
-    view.addObject("rolModel", rolService.roles());
-    view.addObject("programaModel", programaService.programas());
-    return view;
-  }
+	@GetMapping("/add")
+	public ModelAndView create() {
+		ModelAndView view = new ModelAndView(VISTA_EDITAR);
+		view.addObject("usuarioModel", new UsuarioModel());
+		view.addObject("rolModel", rolService.roles());
+		view.addObject("programaModel", programaService.programas());
+		return view;
+	}
 
-  @PostMapping("/")
-  public ModelAndView save(@Valid UsuarioModel usuario, BindingResult bindingResult) {
-    ModelAndView view = new ModelAndView();
-    if (bindingResult.hasErrors()) {
-      List<String> errors = new ArrayList<>();
-      for (ObjectError error : bindingResult.getAllErrors()) {
-        errors.add(error.getDefaultMessage());
-      }
-      if(usuario.getIdUser() != 0) {
-    	  usuario.setEditar(true);
-      }
-      view.setViewName(VISTA_EDITAR);
-      view.addObject("usuarioModel", usuario);
-      view.addObject("errors", errors);
-      view.addObject("rolModel", rolService.roles());
-      view.addObject("programaModel", programaService.programas());
-    }
-    else {
-      usuarioService.guardarUsuario(usuario, false);
-      view.setViewName(REDIRECT_USUARIO);
-    }
-    return view;
+	@PostMapping("/")
+	public ModelAndView save(@Valid UsuarioModel usuario, BindingResult bindingResult) {
+		ModelAndView view = new ModelAndView();
+		if (bindingResult.hasErrors()) {
+			List<String> errors = new ArrayList<>();
+			for (ObjectError error : bindingResult.getAllErrors()) {
+				if (error.getCode().contains("typeMismatch")) {
+					errors.add("El campo " + ((DefaultMessageSourceResolvable) error.getArguments()[0]).getCodes()[1]
+							+ " es numérico...");
+				} else {
+					errors.add(error.getDefaultMessage());
+				}
+			}
+			if (usuario.getIdUser() != 0) {
+				usuario.setEditar(true);
+			}
+			view.setViewName(VISTA_EDITAR);
+			view.addObject("usuarioModel", usuario);
+			view.addObject("errors", errors);
+			view.addObject("rolModel", rolService.roles());
+			view.addObject("programaModel", programaService.programas());
+		} else {
+			usuarioService.guardarUsuario(usuario, false);
+			view.setViewName(REDIRECT_USUARIO);
+		}
+		return view;
 
-  }
+	}
 
-  @GetMapping("/delete/{id}")
-  public String delete(@PathVariable(name = "id") String id) {
-    usuarioService.eliminarUsuarioById(id);
-    return REDIRECT_USUARIO;
-  }
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable(name = "id") String id) {
+		usuarioService.eliminarUsuarioById(id);
+		return REDIRECT_USUARIO;
+	}
 
-  @GetMapping("/cambiarcontrasena")
-  public ModelAndView cambiarContraseña() {
-    ModelAndView view = new ModelAndView("/user/cambiarcontrasena");
-    view.addObject("contrasenaModel", new ContrasenaModel());
-    return view;
+	@GetMapping("/cambiarcontrasena")
+	public ModelAndView cambiarContraseña() {
+		ModelAndView view = new ModelAndView("/user/cambiarcontrasena");
+		view.addObject("contrasenaModel", new ContrasenaModel());
+		return view;
 
-  }
+	}
 
-  @PostMapping("/cambiarcontrasena")
-  public ModelAndView save(@Valid ContrasenaModel contrasena) {
-    ModelAndView view = new ModelAndView();
-    if(contrasena.getNuevaPassword().equals(contrasena.getConfirmacionPassword())){
-    	
-    
-    	
-    }else {
-    	
-    	view.setViewName(REDIRECT_CAMBIARCONTRA);
-    	
-    }
+	@PostMapping("/cambiarcontrasena")
+	public ModelAndView save(@Valid ContrasenaModel contrasena) {
+		ModelAndView view = new ModelAndView();
+		if (contrasena.getNuevaPassword().equals(contrasena.getConfirmacionPassword())) {
 
-    return view;
+		} else {
 
-  }
+			view.setViewName(REDIRECT_CAMBIARCONTRA);
+
+		}
+
+		return view;
+
+	}
 
 }
